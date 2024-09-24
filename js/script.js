554ea3b268b3d7b1c6e965c77c5ec1b3f581a738
Ji-Kwon <jacob.wilkinson16@gmail.com>
2024-09-24 18:36:53 -0400

    simplifying

--- js/script.js
+++ js/script.js
@@ -1,19 +1,9 @@
-// Check if the user has already opened the menu
-if (sessionStorage.getItem('menuOpened')) {
-    // If so, hide the textbox and show the menu
-    document.querySelector('.textbox').style.display = 'none';
-    document.getElementById('menu').style.display = 'block';
-} else {
-    // Otherwise, show the textbox and menu will remain hidden
-    document.getElementById('menu').style.display = 'none';
-}
-
-// Script to toggle menu visibility 
+// Script to toggle menu visibility
 document.getElementById('menuButton').addEventListener('click', function() {
-    // Hide textbox
+    //hide textbox
     document.querySelector('.textbox').style.display = 'none';
-    // Show menu
+    //open menu
     document.getElementById('menu').style.display = 'block';
-    // Set session storage to indicate the menu has been opened
-    sessionStorage.setItem('menuOpened', 'true');
 });
+
+  
