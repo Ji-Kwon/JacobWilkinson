38d32169f87ffa008ca0c4f858f39a313127c0e7
Ji-Kwon <jacob.wilkinson16@gmail.com>
2024-09-24 03:53:46 -0400

    Updates

---
+++ script.js
@@ -0,0 +1,9 @@
+// Script to toggle menu visibility
+document.getElementById('menuButton').addEventListener('click', function() {
+    //hide textbox
+    document.querySelector('.textbox').style.display = 'none';
+    //open menu
+    document.getElementById('menu').style.display = 'block';
+});
+
+  
