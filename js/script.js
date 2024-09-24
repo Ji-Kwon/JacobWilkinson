ee23873c4169519ae50db3a98fdfeca4faa74c19
Ji-Kwon <jacob.wilkinson16@gmail.com>
2024-09-24 18:31:02 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,26 +6,27 @@ function getQueryParameter(name) {
 
 // Check if the menu should be opened based on query parameters
 if (getQueryParameter('openMenu') === 'true') {
-    // If so, hide the textbox and show the menu
     document.querySelector('.textbox').style.display = 'none';
     document.getElementById('menu').style.display = 'block';
-    // Set session storage to indicate the menu has been opened
     sessionStorage.setItem('menuOpened', 'true');
+    history.replaceState(null, '', window.location.pathname);
 } else if (sessionStorage.getItem('menuOpened')) {
-    // If the menu was previously opened in this session, show it
     document.querySelector('.textbox').style.display = 'none';
     document.getElementById('menu').style.display = 'block';
 } else {
-    // Otherwise, show the textbox
     document.getElementById('menu').style.display = 'none';
 }
 
 // Script to toggle menu visibility 
 document.getElementById('menuButton').addEventListener('click', function() {
-    // Hide textbox
     document.querySelector('.textbox').style.display = 'none';
-    // Show menu
     document.getElementById('menu').style.display = 'block';
-    // Set session storage to indicate the menu has been opened
     sessionStorage.setItem('menuOpened', 'true');
+});
+
+// Allow normal navigation for project button
+document.querySelectorAll('.menubutton').forEach(button => {
+    button.addEventListener('click', function(event) {
+        console.log(this.textContent + ' button clicked!');
+    });
 });
